7400eca672fa3013facebd8571d9b60d8318ea7f
mibomboq <rezar93438@gmail.com>
2026-05-25 07:44:03 +0700

    Clean up whitespace in px.sh
    Remove an empty line and adjust formatting.

--- px.sh
+++ px.sh
@@ -8,7 +8,6 @@ git clone https://github.com/mibomboq/local_manifest.git -b master .repo/local_m
 
 export BUILD_USERNAME=bombo
 export BUILD_HOSTNAME=crave
-
 export CUSTOM_MAINTAINER=Doo
 
 source build/envsetup.sh
