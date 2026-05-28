4261a39afa2b031d2b998b329430bec9750ae63f
mibomboq <rezar93438@gmail.com>
2026-05-28 12:40:36 +0700

    Update file check and upload script for PixelOS

--- px.sh
+++ px.sh
@@ -16,8 +16,8 @@ breakfast X1 user
 m pixelos
 
 echo "Upload to gofile will be started..."
-if [ -f out/target/product/X1/PixelOS_X1-16.2-20260527*.zip ]; then
+if [ -f out/target/product/X1/PixelOS_X1-16.2-6*.zip ]; then
     wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-    chmod +x upload.sh ; ./upload.sh out/target/product/X1/PixelOS_X1-16.2-20260527*.zip
+    chmod +x upload.sh ; ./upload.sh out/target/product/X1/PixelOS_X1-16.2-*.zip
 fi
 echo "hame"
