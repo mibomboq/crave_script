8bb56a752207f6d0a36de6526b5033d891af762a
mibomboq <rezar93438@gmail.com>
2026-05-28 14:09:21 +0700

    Add px-wip.sh script for PixelOS build and upload

---
+++ px-wip.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf .repo/local_manifests
+rm -rf device/advan/X1
+rm -rf vendor/advan/X1
+
+repo init --no-repo-verify --git-lfs -u https://github.com/Kitauji-High-School/pixelos_manifest.git -b sixteen-qpr2 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mibomboq/local_manifest.git -b wip .repo/local_manifests
+/opt/crave/resync.sh || repo sync
+
+export BUILD_USERNAME=bombo
+export BUILD_HOSTNAME=crave
+
+source build/envsetup.sh
+breakfast X1 user
+m pixelos
+
+echo "Upload to gofile will be started..."
+if [ -f out/target/product/X1/PixelOS_X1-16.2-6*.zip ]; then
+    wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
+    chmod +x upload.sh ; ./upload.sh out/target/product/X1/PixelOS_X1-16.2-*.zip
+fi
+echo "hame"
