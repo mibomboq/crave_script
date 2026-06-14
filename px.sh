523b0d3afef6b0dc934125ffd1142e9c8459c9bc
mibomboq <rezar93438@gmail.com>
2026-06-14 19:19:37 +0700

    Update px.sh

--- px.sh
+++ px.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-rm -rf prebuilts/clang/host/linux-x86
 rm -rf .repo/local_manifests
 rm -rf device/advan/X1
 rm -rf vendor/advan/X1
 rm -rf vendor/prize/camera
-rm -rf out/soong/.bootstrap out/soong/build.custom_X1.ninja
-
 
 repo init --no-repo-verify --git-lfs -u https://github.com/Kitauji-High-School/pixelos_manifest.git -b sixteen-qpr2 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mibomboq/local_manifest.git -b master .repo/local_manifests
@@ -16,6 +13,9 @@ export BUILD_HOSTNAME=crave
 
 source build/envsetup.sh
 breakfast X1 user
+
+make installclean
+
 m pixelos
 
 echo "Upload to gofile will be started..."
