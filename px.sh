ba3233c0849283213ed6145d8768fc84afb7e1f0
mibomboq <rezar93438@gmail.com>
2026-06-14 19:13:07 +0700

    Clean up build directories and initialize repo
    Remove additional build directories and update repo initialization.

--- px.sh
+++ px.sh
@@ -4,6 +4,8 @@ rm -rf .repo/local_manifests
 rm -rf device/advan/X1
 rm -rf vendor/advan/X1
 rm -rf vendor/prize/camera
+rm -rf out/soong/.bootstrap out/soong/build.custom_X1.ninja
+
 
 repo init --no-repo-verify --git-lfs -u https://github.com/Kitauji-High-School/pixelos_manifest.git -b sixteen-qpr2 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mibomboq/local_manifest.git -b master .repo/local_manifests
