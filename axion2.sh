53ebd7538154a1dfcec5b8e4926f6766424365bf
mibomboq <rezar93438@gmail.com>
2026-06-25 01:32:18 +0700

    Refactor axion2.sh for cleaner build setup
    Removed unnecessary cleanup commands and adjusted build process.

--- axion2.sh
+++ axion2.sh
@@ -2,8 +2,6 @@
 rm -rf .repo/local_manifests
 rm -rf device/advan/X1
 rm -rf vendor/advan/X1
-rm -rf vendor/prize/camera
-rm -rf out/target/product/X1
 
 repo init --no-repo-verify --git-lfs -u https://github.com/AxionAOSP/android.git -b lineage-23.2 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mibomboq/local_manifest.git -b axion .repo/local_manifests
@@ -13,14 +11,10 @@ export BUILD_USERNAME=bombo
 export BUILD_HOSTNAME=crave
 
 . build/envsetup.sh
-
-gk -s
-
 axion X1 user full
 
 make installclean
 
-axionSync
 ax -b
 
 echo "Upload to gofile will be started..."
