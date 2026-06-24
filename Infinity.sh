6ce43e03a4fa3e71bed13a3070bab8351377bc75
mibomboq <rezar93438@gmail.com>
2026-06-25 01:23:20 +0700

    Enhance Infinity.sh with additional cleanup and build steps
    Added cleanup commands and modified build process.

--- Infinity.sh
+++ Infinity.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-rm -rf prebuilts/clang/host/linux-x86
-rm -rf packages/apps/GameBar
-rm -rf device/advan/X1-kernel
+rm -rf .repo/local_manifests
+rm -rf device/advan/X1
+rm -rf vendor/advan/X1
+rm -rf out/target/product/X1
 
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mibomboq/local_manifest.git -b los .repo/local_manifests
 /opt/crave/resync.sh || repo sync
 
-git clone --depth=1 https://github.com/mibomboq/android-device-advan-X1.git -b main device/advan/X1
-git clone --depth=1 https://github.com/mibomboq/android-vendor-advan-X1.git -b main vendor/advan/X1
-git clone --depth=1 https://github.com/G100-X1/android_device_advan_X1-kernel.git -b lineage-23.1 device/advan/X1-kernel
-
-git clone https://github.com/G100-X1/device_mediatek_sepolicy_vndr.git -b lineage-23.1 device/mediatek/sepolicy_vndr
-git clone https://github.com/G100-X1/hardware_mediatek.git -b lineage-23.1 hardware/mediatek
-git clone https://github.com/G100-X1/android_vendor_mediatek_ims.git -b lineage-23.1 vendor/mediatek/ims
-git clone https://github.com/G100-X1/android_vendor_sony_dolby.git -b sixteen-redesign vendor/sony/dolby
-
-export BUILD_USERNAME=miku
-export BUILD_HOSTNAME=crave
+export BUILD_USERNAME=random
+export BUILD_HOSTNAME=kid
 
 . build/envsetup.sh
+
+# run
 lunch infinity_X1-user
+
+make installclean
 m bacon
+
+echo "Upload to gofile will be started..."
+if [ -f out/target/product/X1/Project*X1*.zip ]; then
+    wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
+    chmod +x upload.sh ; ./upload.sh out/target/product/X1/Project*X1*.zip
+fi
+echo "finish_bijh"
