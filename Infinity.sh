87ef32b0cbf7c63817392cc5ebddc54967b27428
mibomboq <rezar93438@gmail.com>
2026-07-08 14:04:27 +0700

    Clean up device directories in Infinity.sh
    Removed specific device directories and added cleanup for Mediatek hardware.

--- Infinity.sh
+++ Infinity.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
-rm -rf device/advan/X1
-rm -rf vendor/advan/X1
-rm -rf device/prize/camera
+rm -rf hardware/mediatek
+rm -rf device/mediatek/sepolicy_vndr
 
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mibomboq/local_manifest.git -b los .repo/local_manifests
