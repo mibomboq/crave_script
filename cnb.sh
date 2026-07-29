bb6253a43acc0893ed6ccfb4d1d4f0e5e3d135ce
mibomboq <rezar93438@gmail.com>
2026-07-29 13:21:41 +0700

    Add script to build and upload EvolutionX for X1
    This script initializes a repo, cleans up specific directories, and builds the EvolutionX ROM for the X1 device. It also includes a file upload step for the generated ROM.

---
+++ cnb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+rm -rf .repo/local_manifests
+rm -rf device/advan/X1
+rm -rf vendor/advan/X1
+rm -rf hardware/mediatek
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf kernel/advan
+
+repo init -u https://github.com/Evolution-X/manifest -b cnb --git-lfs --depth=1
+git clone https://github.com/mibomboq/local_manifest.git -b 17 .repo/local_manifests
+/opt/crave/resync.sh || repo sync
+
+export BUILD_USERNAME=random
+export BUILD_HOSTNAME=kid
+
+. build/envsetup.sh
+
+# run
+lunch lineage_X1-cp2a-user
+
+make installclean
+m evolution
+
+echo "Upload to gofile will be started..."
+if [ -f out/target/product/X1/EvolutionX-17.0*.zip ]; then
+    wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
+    chmod +x upload.sh ; ./upload.sh out/target/product/X1/EvolutionX-17.0*.zip
+fi
+echo "Finish"
