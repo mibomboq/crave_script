7723096b81b40e01d2cfc43e4179a6350d13e19a
mibomboq <rezar93438@gmail.com>
2026-08-05 06:56:42 +0700

    Update branch and version for EvolutionX build script

--- tes.sh
+++ tes.sh
@@ -6,7 +6,7 @@ rm -rf device/mediatek/sepolicy_vndr
 rm -rf vendor/advan/X1
 
 
-repo init -u https://github.com/Evolution-X/manifest -b cnb --git-lfs --depth=1
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs --depth=1
 git clone https://github.com/mibomboq/local_manifest.git -b 17 .repo/local_manifests
 /opt/crave/resync.sh || repo sync
 
@@ -16,14 +16,14 @@ export BUILD_HOSTNAME=kid
 . build/envsetup.sh
 
 # run
-lunch lineage_X1-cp2a-user
+lunch lineage_X1-bp4a-user
 
 make installclean
 m evolution
 
 echo "Upload to gofile will be started..."
-if [ -f out/target/product/X1/EvolutionX-17.0*.zip ]; then
+if [ -f out/target/product/X1/EvolutionX-16.0*.zip ]; then
     wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-    chmod +x upload.sh ; ./upload.sh out/target/product/X1/EvolutionX-17.0*.zip
+    chmod +x upload.sh ; ./upload.sh out/target/product/X1/EvolutionX-16.0*.zip
 fi
 echo "finish_bijh"
