786732977cfbbd142b057f54c8c7123d47af4930
mibomboq <rezar93438@gmail.com>
2026-08-13 12:36:01 +0700

    Clean up local manifests and update repo init
    Remove unnecessary directories and update repo initialization.

--- Infinity.sh
+++ Infinity.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
+rm -rf device/advan
+rm -rf vendor/advan
 
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mibomboq/local_manifest.git -b los .repo/local_manifests
