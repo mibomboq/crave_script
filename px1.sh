6eb6d3e1cc942338017d60e3cfa0d333fc0ba828
mibomboq <rezar93438@gmail.com>
2026-09-01 05:34:46 +0700

    Update px1.sh

--- px1.sh
+++ px1.sh
@@ -2,6 +2,7 @@
 rm -rf .repo/local_manifests
 rm -rf device/advan/X1
 rm -rf vendor/advan/X1
+rm -rf device/advan/X1-kernel
 
 repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b seventeen --git-lfs --depth=1
 git clone https://github.com/mibomboq/local_manifest.git -b 17 .repo/local_manifests
