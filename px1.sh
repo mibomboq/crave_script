789eb2923daa10cd029ee67a43944029e13afd88
mibomboq <rezar93438@gmail.com>
2026-08-31 09:08:38 +0700

    Add Go environment variables to px1.sh
    Set environment variables for Go memory management.

--- px1.sh
+++ px1.sh
@@ -7,6 +7,7 @@ git clone https://github.com/mibomboq/local_manifest.git -b 17 .repo/local_manif
 
 export BUILD_USERNAME=bombo
 export BUILD_HOSTNAME=crave
+export GOMAXPROCS=16 GOMEMLIMIT=42GiB GOGC=25 MALLOC_ARENA_MAX=4
 
 # run this line after resync
    wget https://github.com/yaap-17-stone/build_soong/raw/f9c27b0b9298f6eeee9a850346e0a646c3eaeb87/cmd/soong_build/main.go && mv main.go build/soong/cmd/soong_build/
