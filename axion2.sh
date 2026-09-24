7bcdc1b86bf90b6bbd0768814f6c0ee36ba688b1
mibomboq <rezar93438@gmail.com>
2026-09-25 03:26:48 +0700

    Remove camera directory and update repo initialization
    Remove camera directory from vendor and clean up repo.

--- axion2.sh
+++ axion2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
 rm -rf device/advan/X1
+rm -rf vendor/prize/camera
 
 repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs --depth=1
 git clone https://github.com/mibomboq/local_manifest.git -b los .repo/local_manifests
